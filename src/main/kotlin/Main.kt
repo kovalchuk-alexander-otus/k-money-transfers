e7e2cfe3c4f26e08bc81ac07e543108f144ad9bc
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-11-29 01:19:50 +0300

    fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val commissionPercent: Float = 0.75F; // процент комисии с суммы перевода
-    val commissionMin: Int = 35; // минимальная сумма комиссии
+    val commissionPercent = 0.75F // процент комисии с суммы перевода
+    val commissionMin = 35 // минимальная сумма комиссии
 
     /**
      * рассчет суммы комиссии по сумме перевода
@@ -8,7 +8,7 @@ fun main() {
      * amount - сумма перевода
      */
     fun calcCommission(amount: Int): Int {
-        val calc = (amount*commissionPercent/100).toInt();
+        val calc = (amount*commissionPercent/100).toInt()
         return if (calc > commissionMin) calc else commissionMin
     }
 
